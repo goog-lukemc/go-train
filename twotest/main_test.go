12c9c9226181a06b2c29f79054a7cbbe11a57879
Luke <lukemc@google.com>
2018-08-16 08:28:33 -0400

    ready for dl

---
+++ twotest/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	tests := []struct {
+		url    string
+		result string
+	}{
+		{"http://www.homedepot.com/findit", "findit"},
+		{"http://google.com/search", "search"},
+		{"http://bo.bo.com/one/two/shoe/", "shoe"},
+		{"http://youtube.com/path/to/file?is=12345", "file"},
+	}
+
+	for _, test := range tests {
+		base := GetPathBase(test.url)
+		if base != test.result {
+			t.Errorf("Nope - Check out the path package!, got: %d, want: %d.", test.x, test.y, test, test.n)
+		}
+	}
+}
