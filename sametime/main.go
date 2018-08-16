0e0ddef06692ae1bc9859126536a9d40bf4dad58
Luke <lukemc@google.com>
2018-08-16 00:46:25 -0400

    ready for dl

--- sametime/main.go
+++ sametime/main.go
@@ -11,21 +11,30 @@ import (
 	"github.com/google/uuid"
 )
 
+// These are our flag variables
 var workers = flag.Int("workers", 1, "Number of Workers")
 var testcount = flag.Int("datastrings", 1, "Number of Data Strings")
 
+// Wait group from the sync package
 var wg sync.WaitGroup
 
 func main() {
+	// Setting a time so we can calculate Duration
 	start := time.Now()
+
+	// dont forget to parse you flags
 	flag.Parse()
+
+	// make a channel to safely communicate with our workders
 	s := make(chan string)
 
+	// Create a worker pool
 	for i := 0; i < *workers; i++ {
 		wg.Add(1)
 		go writeFile(s)
 	}
 
+	// create a UUID and set it on the channel
 	for x := 0; x < *testcount; x++ {
 		ud, err := uuid.NewUUID()
 		if err != nil {
@@ -35,21 +44,26 @@ func main() {
 		s <- ud.String()
 	}
 
+	// make sure the channel is empty before we close it
+	// this helps us support buffered channels
 	if len(s) == 0 {
 		close(s)
 	}
 
+	// waits for all our workers to finish
 	wg.Wait()
 	f := time.Since(start)
 	log.Printf("Workload Count:%v Goroutine Count:%v Duration:%v", *workers, *testcount, f)
 }
 
 func writeFile(itemschan chan string) {
+	// range the channel watching as the items come in
 	for data := range itemschan {
 		err := ioutil.WriteFile("/Users/ldeakm/temp/"+data+".txt", []byte(data), 0644)
 		if err != nil {
 			log.Printf("file error%v", err.Error())
 		}
 	}
+	// gorouting is exiting
 	wg.Done()
 }
